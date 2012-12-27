032cf55f9666e78830328007ce35c6607f6bc141
Sachin Maharjan <sachin.gen@gmail.com>
2012-12-26 20:43:14 -0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Callbreak::Application.config.secret_token = '2b84e438acb12252c0cfa4d740d42ca75cbf00346487426f9bffc3b48c5e6f1d6b7a414a7e94c7a9164b44d88d37ea280dadb95bcd931c479254d2a5f88ebe1d'
